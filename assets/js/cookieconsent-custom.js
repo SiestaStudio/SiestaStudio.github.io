d934ba2c704f2365223dd0d269807b1c06ff7267
siestastudio <anibalrg@siesta.studio>
2019-03-15 15:56:41 +0100

    Cookie consent added

---
+++ assets/js/cookieconsent-custom.js
@@ -0,0 +1,17 @@
+
+window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+        "palette": {
+            "popup": {
+                "background": "#000000"
+            },
+            "button": {
+                "background": "#bcdde2"
+            }
+        },
+        "content": {
+            "dismiss": "Got it!",
+            "link": "More about privacy",
+            "href": "/privacy/"
+        }
+    })});
